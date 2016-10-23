3d7279b6b8a636a4abd99b96a65edd782ead16b7
Chaitanya Sri Krishna Lolla <Chaitanya Sri Krishna Lolla>
2016-10-23 14:29:11 -0400

    Added User Login Implementation.

---
+++ src/com/clothingcloset/api/Login.java
@@ -0,0 +1,12 @@
+package com.clothingcloset.api;
+
+import com.clothingcloset.models.UserLogin;
+
+public interface Login {
+
+	public boolean registerUser(UserLogin userLogin);
+	
+	public boolean validateUser(UserLogin userLogin);
+	
+	
+}
